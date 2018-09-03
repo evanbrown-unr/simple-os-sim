86bc1164b3e2ff7ff09589037405861b55cfe72f
Evan Brown <evanbrown@nevada.unr.edu>
2018-09-03 01:45:04 -0700

    Finished writing Configuration module. It compiles but has not undergone unit testing.

--- Phase-One/Configuration.java
+++ Phase-One/Configuration.java
@@ -2,7 +2,10 @@
 /**
  * This is the configuration module of the OS simulation.
  * It stores the current simulation settings, as well performing
- * the various operations on it.
+ * the various operations on it. I've written the read method to
+ * handle config files that are only in the shown format.
+ * In the future, I hope to improve flexibility and performance
+ * by using regular expressions.
  */
 
 import java.io.FileInputStream;
@@ -64,7 +67,7 @@ public class Configuration
      *          must be called before using any of its functionalities.
      * \param configFilePath The file path for the configuration file.
      */
-    public static void init(String configFilePath) throws FileNotFoundException
+    public static void init(String configFilePath) throws FileNotFoundException, IOException
     {
         version = new String();
         mdfPath = new String();
@@ -135,11 +138,64 @@ public class Configuration
         }
     }
 
-    public static boolean readConfigFile(String configFilePath) throws FileNotFoundException
+    /**
+     * \brief Reads the configuration file and sets configuration
+     *        settings.
+     * \details Called in class's initialization method.
+     * \param A string containing the configuration file path.
+     */
+    private static void readConfigFile(String configFilePath) throws FileNotFoundException, IOException
     {
         FileInputStream configFile = new FileInputStream(configFilePath);
+        Scanner lineScan = new Scanner(configFile);
+
+        lineScan.nextLine(); // ignore first line
+
+        version = extractOption(lineScan);
+        mdfPath = extractOption(lineScan);
+
+        // time values must be integers
+        processorTime = Integer.parseInt(extractOption(lineScan));
+        monitorTime = Integer.parseInt(extractOption(lineScan));
+        hardDriveTime = Integer.parseInt(extractOption(lineScan));
+        printerTime = Integer.parseInt(extractOption(lineScan));
+        keyboardTime = Integer.parseInt(extractOption(lineScan));
+
+        // must determine log type with switch statement
+        String logTypeString = extractOption(lineScan);
+        logFilePath = extractOption(lineScan);
+
+        switch (logTypeString)
+        {
+            case "Log to Both":
+                logType = LogType.BOTH;
+                break;
+            case "Log to Monitor":
+                logType = LogType.MONITOR;
+                break;
+            case "Log to File":
+                logType = LogType.FILE;
+                break;
+            default:
+                Logger.logError("Failed to read log type");
+        }
+
+        configFile.close();
+        lineScan.close();
     }
 
-    public static void getNextOption(Scanner lineScan, String currentLine)
-    {}
+    /**
+     * \brief Helper function to parse configuration line
+     *        and return the relevant data;
+     * \details A lot is going on in that one line of code.
+     *          First the line is read fron the scanner, then
+     *          it is split at the colon. Since there is still a
+     *          whitespace in front of the data string, it gets trimmed.
+     * \param lineScan The Scanner that is attached to the config file.
+     * \returns A string containing the valid option data.
+     */
+    private static String extractOption(Scanner lineScan)
+    {
+        return lineScan.nextLine().split(":")[1].substring(1);
+    }
 }
