a0eb68983416e943e0b76d42cfe529b6826a8ef1
Evan Brown <evanbrown@nevada.unr.edu>
2018-10-20 08:08:12 -0700

    Added comments

--- Phase-3/ResourceManager.java
+++ Phase-3/ResourceManager.java
@@ -1,3 +1,11 @@
+/**
+ * This class contains the resource management module. This controls the
+ * simulation's access to hardware resources by defining a critical section
+ * with the use of semaphore locks. This is definitely not an ideal algorithm, and
+ * works best on simulations that run sequentially and do not perform time slicing.
+ * I am planning on refactoring this module for the next phase.
+ */
+
 import java.util.concurrent.Semaphore;
 import java.util.LinkedList;
 
@@ -8,6 +16,10 @@ public class ResourceManager
     private static int projectorIndex = 0;
     private static int hardDriveIndex = 0;
 
+    /**
+     * \brief Initializs necessary fields before using any class utlities.
+     * \details Allocates memory to each object in the arrays.
+     */
     public static void init()
     {
         for (int i = 0; i < Configuration.projectorQuantity; i++)
@@ -17,6 +29,11 @@ public class ResourceManager
             hardDriveLocks[i] = new Semaphore(1);
     }
 
+    /**
+     * \brief acquires the next available resource available.
+     * \details After being acquired, the resource must be released,
+     *          in order to use it again throughout the simulation.
+     */
     public static void acquireResource(Operation op)
     {
         switch (op.name)
@@ -24,6 +41,7 @@ public class ResourceManager
             case "projector":
                 projectorLocks[projectorIndex].acquireUninterruptibly();
                 break;
+
             case "hard drive":
                 hardDriveLocks[hardDriveIndex].acquireUninterruptibly();
                 break;
@@ -31,6 +49,10 @@ public class ResourceManager
         }
     }
 
+    /**
+     * \brief Releases an acquired resource.
+     * \details
+     */
     public static void releaseResource(Operation op)
     {
         switch (op.name)
@@ -41,6 +63,7 @@ public class ResourceManager
                     projectorIndex = 0;
                 else projectorIndex++;
                 break;
+
             case "hard drive":
                 hardDriveLocks[hardDriveIndex].release();
                 if (hardDriveIndex == Configuration.hardDriveQuantity-1)
@@ -51,11 +74,17 @@ public class ResourceManager
         }
     }
 
+    /**
+     * \brief Returns the current projector in use.
+     */
     public static int getCurrentProjector()
     {
         return projectorIndex;
     }
 
+    /**
+     * \brief Returns the current hardDrive in use.
+     */
     public static int getCurrentHardDrive()
     {
         return hardDriveIndex;
